e0b85b102b4da12429b6c1b033a73a22fbc151b6
hiroyone <hiroki.yonetani@zalando.de>
2022-11-06 23:31:37 +0100

    implement get_file_paths_by_glob

--- cypress-parallel/src/lib.rs
+++ cypress-parallel/src/lib.rs
@@ -1,4 +1,5 @@
 use std::{path::{Path, PathBuf}, fs, io};
+use glob::{PatternError};
 
 /// Get a list of file paths under the directory
 ///
@@ -12,4 +13,15 @@ fn get_file_paths_by_dir_path (dir_path:&Path) -> Result<Vec<PathBuf>, io::Error
     
     entries.sort();
     return Ok(entries)
+}
+
+fn get_file_paths_by_glob(pattern:&str) -> Result<Vec<PathBuf>, PatternError>{
+
+    let mut entries = glob::glob(pattern).expect("Failed to read glob pattern")
+        .filter_map(Result::ok)
+        .collect::<Vec<PathBuf>>();
+
+    entries.sort();
+    return Ok(entries)
+
 }
