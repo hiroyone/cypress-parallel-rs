b99359553877357c3f30183d53bae06d156f2493
hiroyone <hiroki.yonetani@zalando.de>
2022-11-06 14:42:19 +0100

    Implement weight function

--- cypress-parallel/src/main.rs
+++ cypress-parallel/src/main.rs
@@ -1,5 +1,4 @@
-use std::{path::Path, fs, io::Result, process};
-
+use std::{path::Path, fs, io::Result, process, time::Duration, collections::HashMap};
 
 /// Remove all files in the directory only if the directory exists. Do nothing if not.
 ///
@@ -15,10 +14,46 @@ fn clean_directory(dir_path:&str) -> Result<()> {
     return Ok(())
 }
 
+#[derive(Debug)]
+struct SpecWeight {
+    time: Duration,
+    weight: u64
+}
+type SpecWeights<'a> = HashMap<&'a str, SpecWeight>;
+type TotalWeight = u64;
+
+/// Create weights for parallel computing based on the ratio of an execution time to the total time
+fn generate_weights (spec_weights:&mut SpecWeights, total_duration:Duration, total_weight:TotalWeight) {
+    for (_, spec_weight) in spec_weights.iter_mut() {
+        let ratio = (spec_weight.time.as_millis() / total_duration.as_millis()) as u64 ;
+        spec_weight.weight =((ratio * total_weight) as f64).floor() as u64 ;
+    }
+}
+
+#[test]
+fn generate_weights_test() {
+
+    let mut spec_weights:SpecWeights = HashMap::from([
+        ("sample", SpecWeight {
+            time: Duration::from_millis(500),
+            weight:0
+        })
+    ]) ;
+
+    let total_weight:TotalWeight = 1000;
+
+    generate_weights(&mut spec_weights, Duration::from_millis(100), total_weight);
+
+    assert_eq!(spec_weights["sample"].weight, 5000);
+}
 
 fn main() {
     if let Err(e) = clean_directory("distdwamo") {
         println!("Application Error: {}", e);
         process::exit(1)
     }
+
+    let duration = Duration::from_millis(2569210323242);
+    println!("{:?}", duration);
+
 }
