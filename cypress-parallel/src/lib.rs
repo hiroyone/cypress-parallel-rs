d8e9c0be4514d135dd4c612a6cfdc60762691251
hiroyone <hiroki.yonetani@zalando.de>
2022-11-06 16:09:37 +0100

    implement get_file_paths_by_dir_path

---
+++ cypress-parallel/src/lib.rs
@@ -0,0 +1,15 @@
+use std::{path::{Path, PathBuf}, fs, io};
+
+/// Get a list of file paths under the directory
+///
+/// # Errors
+///
+/// This function will return an error if .
+fn get_file_paths_by_dir_path (dir_path:&Path) -> Result<Vec<PathBuf>, io::Error>{
+    let mut entries = fs::read_dir(".")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+    
+    entries.sort();
+    return Ok(entries)
+}
