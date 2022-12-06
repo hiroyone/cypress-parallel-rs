7bc809ec1ad428b5e535f10dbbe9b169fe6773ed
hiroyone <hiroki.yonetani@zalando.de>
2022-12-06 21:35:49 +0100

    Implement create_reporter_config_file

--- threads/src/lib.rs
+++ threads/src/lib.rs
@@ -1,5 +1,6 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::PathBuf, fs, io::Result};
 
+use convert_case::{Casing, Case};
 pub enum PackageManager {
     Yarn,
     Npm
@@ -33,3 +34,34 @@ pub fn create_reporter_options(string:&str) -> HashMap<&str, &str> {
     option_map
 } 
 
+/// Write Cypress reporting config to the json file 
+///
+/// # Errors
+///
+/// This function will return an error if writing the JSON to the file fails. 
+pub fn create_reporter_config_file(path:&PathBuf) -> Result<()> {
+    
+    // Todo: Rewrite this once config part is implemented.
+    let settings: HashMap<&str, &str> = HashMap::new();
+
+    let mut reporter_enabled = Vec::from(["cypress-parallel-rs/json-stream.reporter.js"]);
+
+    if settings.contains_key("reporter") {
+        reporter_enabled.push(settings["reporter"])
+    } else {
+        reporter_enabled.push("cypress-parallel/simple-spec.reporter.js")
+    }
+
+    let mut option_name=String::new();
+    if settings.contains_key("reporterOptions") {
+        // Create a camel name + suffix 
+        option_name.push_str(&settings["reporter"].to_case(Case::Camel));
+        option_name.push_str("ReporterOptions");
+    }
+
+    fs::write(path, serde_json::json!({
+        "reporterEnabled": reporter_enabled.join(","),
+        option_name: create_reporter_options(settings["reporterOptions"])
+    }).to_string())
+
+}
