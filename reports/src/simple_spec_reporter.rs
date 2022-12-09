bab62f57dcfa1c647598ac37646119b686e6b6ce
hiroyone <hiroki.yonetani@zalando.de>
2022-12-09 22:00:29 +0100

    Implement get_test_description

--- reports/src/simple_spec_reporter.rs
+++ reports/src/simple_spec_reporter.rs
@@ -3,22 +3,31 @@ use serde_json::{Map, Value};
 type CurrentSuite = Map<String, Value>;
 
 
-pub fn json_stream_custom() {
-    todo!()
-} 
-
-fn get_suite_titles(current_suite:& CurrentSuite) -> String{
-    let mut result = String::new();
-
+/// Generate a test description from a test suite and runner
+fn get_test_description(current_suite:& CurrentSuite, test:&Map<String, Value>) -> String {
+    
+    let mut suite_titles = String::new();
+    // Concat all titles by recursion 
     let mut current = current_suite.clone();
-
     while current.contains_key("parent") {
-        result = format!("{} - {}", current_suite["title"], result) ;
+        suite_titles = format!("{} - {}", current["title"], suite_titles) ;
 
         if let Value::Object(map) = &current["parent"] {
             current =  map.clone();
         }
     }
-    return result
 
+    let test_title = &test["title"];
+    // Todo: Get a runner instance 
+    let runner_suite_file = String::new();
+    
+    return format!("{}{} ({})",suite_titles, test_title, runner_suite_file)
 }
+
+pub fn json_stream_custom() {
+    let current_suite = CurrentSuite::new();
+    let mut test:Map<String, Value> = Map::new();
+    test.insert("title".to_owned(), Value::String("Great".to_owned()));
+    let _test_description = get_test_description(&current_suite, &test, );
+    todo!()
+} 
