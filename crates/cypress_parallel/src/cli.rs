e9e8cb3dcf24f49109e66f8e2c0043cc534f2a91
hiroyone <hiroki.yonetani@zalando.de>
2022-12-24 13:47:30 +0100

    Fix: borrow failure

--- crates/cypress_parallel/src/cli.rs
+++ crates/cypress_parallel/src/cli.rs
@@ -8,13 +8,13 @@ use std::error::Error;
 
 pub async fn start() -> Result<(), Box<dyn Error>> {
     let setting = config::Settings::global();
-    let results_path = setting.results_path;
-    utility::clean_directory(&results_path)?;
+    let results_path = &setting.results_path;
+    utility::clean_directory(results_path)?;
 
     let test_weight_threads = test_suites::get_test_weight_threads()?;
     threads::parallel_execute_threads(test_weight_threads).await?;
 
-    let results_maps = utility::collect_cy_results(&results_path)?;
+    let results_maps = utility::collect_cy_results(results_path)?;
 
     println!("{:?}", results_maps);
 
