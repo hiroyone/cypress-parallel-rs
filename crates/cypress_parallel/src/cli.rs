cc939df3632f80764af070e5d185d313e0a17ff9
hiroyone <hiroki.yonetani@zalando.de>
2023-02-03 18:25:02 +0100

    Clean the CLI file

--- crates/cypress_parallel/src/cli.rs
+++ crates/cypress_parallel/src/cli.rs
@@ -9,8 +9,8 @@ use std::fs;
 pub async fn start() -> Result<(), Box<dyn Error>> {
     let settings = config::Settings::global();
     log::debug!("Reading config parameters: {:?}", settings);
-
     let results_path = &settings.results_path;
+
     utility::clean_directory(results_path)?;
 
     let test_weight_threads = test_suites::get_test_weight_threads()?;
@@ -21,13 +21,16 @@ pub async fn start() -> Result<(), Box<dyn Error>> {
 
     let all_reports = report::bundle_all_reports(&test_results);
     let total_result = report::create_total_result(&test_results);
+    log::trace!("The total result: {:?}", total_result);
+
     let spec_weights = utility::generate_spec_weights(&test_results, total_result.duration);
+    log::trace!("The spec_weights: {:?}", spec_weights);
     let spec_weights_json = serde_json::to_string_pretty(&spec_weights).unwrap();
     fs::write(&settings.weights_json, spec_weights_json)?;
 
     let result_table = report::create_test_result_table(&total_result, &all_reports);
     // Todo: should be printed into the cli without debug
-    log::trace!("The result table is: {:?}", result_table);
+    log::trace!("The result table: {:?}", result_table);
 
     Ok(())
 }
